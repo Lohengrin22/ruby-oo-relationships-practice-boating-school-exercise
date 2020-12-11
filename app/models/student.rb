a25c0d2dd63adf794c895d4afca52dd42cc20a5f
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-11 14:35:36 -0500

    commit tree e1ae320e47aeb95c186011a894e6037d0f702801

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,19 @@
 class Student
 
+    attr_reader :name 
+@@all = []
+
+    def initialize(first_name)
+        @name = first_name
+        @@all << self 
+    end 
+
+    def add_boating_test
+        
+    end
+
+    def self.all
+        @@all 
+    end 
+
 end
